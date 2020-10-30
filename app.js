52b680a136d9da0b346494ee6d7af8eb6d8f8edd
RezaFirouzii <sr2000f@gmail.com>
2020-10-31 01:12:21 +0330

    html and js of the app are completed.

---
+++ app.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+const text = document.getElementById('text');
+
+const totalTime = 10000;
+const breathTime = (totalTime / 5) * 2;
+const holdTime = totalTime / 5;
+
+breatheAnimation();
+
+function breatheAnimation() {
+    text.innerHTML = 'Breath!';
+    container.className = 'container grow';
+
+    setTimeout(() => {
+        text.innerText = 'Hold!';
+        setTimeout(() => {
+            text.innerText = 'Breathe out!';
+            container.className = 'container shrink';
+        }, holdTime);
+    }, breathTime);
+}
+
+setInterval(breatheAnimation, totalTime);
